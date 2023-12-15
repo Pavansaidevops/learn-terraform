d097ada63fef8553fe19969f6773244fee3d02ec
Pavan Sai <pavansaidevops7@gmail.com>
2023-12-15 13:22:49 +0530

    terraform-roboshop dns record

--- roboshop-v1/roboshop.tf
+++ roboshop-v1/roboshop.tf
@@ -8,15 +8,30 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z09825891XTM08RW95ES0"
+  name    = "frontend-dev.pavansai.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mongodb" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-01d1ac98da4c430e4" ]
 
-
   tags = {
     Name = "mongodb"
   }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z09825891XTM08RW95ES0"
+  name    = "mongodb-dev.pavansai.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
 }
 
 resource "aws_instance" "catalogue" {
@@ -24,10 +39,17 @@ resource "aws_instance" "catalogue" {
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-01d1ac98da4c430e4" ]
 
-
   tags = {
     Name = "catalogue"
   }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z09825891XTM08RW95ES0"
+  name    = "catalogue-dev.pavansai.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
 }
 
 resource "aws_instance" "redis" {
@@ -35,10 +57,17 @@ resource "aws_instance" "redis" {
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-01d1ac98da4c430e4" ]
 
-
   tags = {
     Name = "redis"
   }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z09825891XTM08RW95ES0"
+  name    = "redis-dev.pavansai.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
 }
 
 resource "aws_instance" "user" {
@@ -46,10 +75,17 @@ resource "aws_instance" "user" {
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-01d1ac98da4c430e4" ]
 
-
   tags = {
     Name = "user"
   }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z09825891XTM08RW95ES0"
+  name    = "user-dev.pavansai.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
 }
 
 resource "aws_instance" "cart" {
@@ -57,10 +93,17 @@ resource "aws_instance" "cart" {
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-01d1ac98da4c430e4" ]
 
-
   tags = {
     Name = "cart"
   }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z09825891XTM08RW95ES0"
+  name    = "cart-dev.pavansai.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
 }
 
 resource "aws_instance" "mysql" {
@@ -68,10 +111,17 @@ resource "aws_instance" "mysql" {
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-01d1ac98da4c430e4" ]
 
-
   tags = {
     Name = "mysql"
   }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z09825891XTM08RW95ES0"
+  name    = "frontend-dev.mysql.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
 }
 
 resource "aws_instance" "shipping" {
@@ -79,10 +129,17 @@ resource "aws_instance" "shipping" {
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-01d1ac98da4c430e4" ]
 
-
   tags = {
     Name = "shipping"
   }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z09825891XTM08RW95ES0"
+  name    = "shipping-dev.pavansai.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
 }
 
 resource "aws_instance" "rabbitmq" {
@@ -90,10 +147,17 @@ resource "aws_instance" "rabbitmq" {
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-01d1ac98da4c430e4" ]
 
-
   tags = {
     Name = "rabbitmq"
   }
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z09825891XTM08RW95ES0"
+  name    = "rabbitmq-dev.pavansai.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
 }
 
 resource "aws_instance" "payment" {
@@ -101,10 +165,17 @@ resource "aws_instance" "payment" {
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-01d1ac98da4c430e4" ]
 
-
   tags = {
     Name = "payment"
   }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z09825891XTM08RW95ES0"
+  name    = "payment-dev.pavansai.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
 }
 
 resource "aws_instance" "dispatch" {
@@ -112,9 +183,17 @@ resource "aws_instance" "dispatch" {
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-01d1ac98da4c430e4" ]
 
-
   tags = {
     Name = "dispatch"
   }
 }
+
+resource "aws_route53_record" "dispatch" {
+  zone_id = "Z09825891XTM08RW95ES0"
+  name    = "dispatch-dev.pavansai.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.dispatch.private_ip]
+}
+
 
