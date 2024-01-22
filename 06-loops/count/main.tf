d8887ddfa0dfff2eedea3b635c103357f4ac77cd
Pavan Sai <pavansaidevops7@gmail.com>
2024-01-22 16:16:21 +0530

    loops count

---
+++ 06-loops/count/main.tf
@@ -0,0 +1,22 @@
+variable "components" {
+  default = ["frontend", "mongodb"]
+}
+
+resource "aws instance" "instance" {
+
+  count = length(var.components)
+
+  ami = "ami-0f3c7d07486cad139"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-01d1ac98da4c430e4" ]
+
+  tags = {
+    //Name = var.components[count.index]
+    Name = element(var.components, count.index)
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  count = length(var.components)
+  name = element(var.components, count.index)
+}
