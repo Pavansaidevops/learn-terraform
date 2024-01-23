367063d52b66351cbf18e9ee589384cfe11ae688
Pavan Sai <pavansaidevops7@gmail.com>
2024-01-23 09:29:33 +0530

    roboshop v2

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -43,7 +43,7 @@ resource "aws_instance" "instance" {
 resource "aws_route53_record" "record" {
   for_each = var.components
   zone_id = var.zone_id
-  name    = "${lookup(each.value, "name", null)}pavansai.online"
+  name    = "${lookup(each.value, "name", null)}.pavansai.online"
   type    = "A"
   ttl     = 30
   records = [lookup(lookup(aws_instance.instance, each.key, null), "private_ip", null) ]
