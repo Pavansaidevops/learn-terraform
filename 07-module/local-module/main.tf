f898dbbfbc8a654d6569c3a0a50f3941d889d5ae
Pavan Sai <pavansaidevops7@gmail.com>
2024-01-23 10:08:52 +0530

    terraform module

---
+++ 07-module/local-module/main.tf
@@ -0,0 +1,12 @@
+module "test" "ami" {
+  source = "./local-module"
+  instance_type = var.instance_type
+}
+
+output "test" {
+  value = module.test
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
