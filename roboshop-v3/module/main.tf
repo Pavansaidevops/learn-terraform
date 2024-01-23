2c5d962f3c6b0627d9b435dc36eff5946936b0fc
Pavan Sai <pavansaidevops7@gmail.com>
2024-01-23 15:14:38 +0530

    roboshop v3

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -17,3 +17,19 @@ resource "aws_route53_record" "record" {
   ttl     = 30
   records = [aws_instance.instance.private_ip ]
 }
+
+resource "null_resource" "ansible" {
+
+  depends_on = [
+    aws_route53_record.record
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd root/roboshop-ansible/
+git pull
+sleep 30
+ansible-playbook -i ${var.name}-dev.pavansai.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+EOF
+  }
+}
